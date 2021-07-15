a966440597e684fb266cf8fe0021fcdf2595a695
Angello Hernandez <a.gianni1234@gmail.com>
2021-07-15 13:44:07 -0700

    Implemented flashcard flip animation

--- app/src/main/java/com/example/lingwa/FlashcardsActivity.java
+++ app/src/main/java/com/example/lingwa/FlashcardsActivity.java
@@ -2,30 +2,43 @@ package com.example.lingwa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lingwa.util.Translator;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.Executor;
 
 public class FlashcardsActivity extends AppCompatActivity {
 
     private static final String TAG = "FlashcardsActivity";
     ArrayList<String> savedWords;
     ProgressBar pbFlashcardProgress;
+    FrameLayout flFlashcard;
     TextView tvWord;
     EditText etAnswer;
     Button btnSubmit;
+
     String currentWord;
     String wordTranslation = null;
     int wordIndex = 0;
+    boolean flashcardFlipped = false;
+    Context context = this;
+
+    final long FLIP_DURATION = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +47,7 @@ public class FlashcardsActivity extends AppCompatActivity {
 
         savedWords = getIntent().getStringArrayListExtra("savedWords");
         pbFlashcardProgress = findViewById(R.id.pbFlashcardProgress);
+        flFlashcard = findViewById(R.id.flFlashcard);
         tvWord = findViewById(R.id.tvWord);
         etAnswer = findViewById(R.id.etAnswer);
         btnSubmit = findViewById(R.id.btnSubmit);
@@ -43,6 +57,8 @@ public class FlashcardsActivity extends AppCompatActivity {
         nextWord();
 
         btnSubmit.setOnClickListener(submit);
+        flFlashcard.setOnClickListener(flipFlashcard);
+
 
     }
 
@@ -57,10 +73,13 @@ public class FlashcardsActivity extends AppCompatActivity {
                 if (answer.equals(wordTranslation)) {
                     wordIndex++;
                     if (wordIndex > savedWords.size() - 1) {
+                        Toast.makeText(context, "Practice complete!", Toast.LENGTH_SHORT).show();
+                        finish();
                         return;
                     }
                     wordTranslation = null;
                     int progress = (int) (((float) wordIndex / (float) savedWords.size()) * 100);
+                    flashcardFlipped = false;
                     pbFlashcardProgress.setProgress(progress);
                     currentWord = savedWords.get(wordIndex);
                     etAnswer.setText("");
@@ -69,10 +88,39 @@ public class FlashcardsActivity extends AppCompatActivity {
             }
     };
 
+    View.OnClickListener flipFlashcard = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (!flashcardFlipped) {
+                flashcardFlipped = true;
+                flFlashcard.animate().rotationY(180f).setDuration(FLIP_DURATION).start();
+                tvWord.animate().rotationY(180f).setDuration(FLIP_DURATION).start();
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvWord.setText(wordTranslation);
+                    }
+                }, FLIP_DURATION/2);
+            } else {
+                flashcardFlipped = false;
+                flFlashcard.animate().rotationY(360f).setDuration(FLIP_DURATION).start();
+                tvWord.animate().rotationY(360f).setDuration(FLIP_DURATION).start();
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvWord.setText(currentWord);
+                    }
+                }, FLIP_DURATION/2);
+            }
+        }
+    };
+
     private void nextWord() {
         Translator.translateWord(currentWord, "es", "en", new Translator.TranslatorCallback() {
             @Override
             public void onTranslationSuccess(String translation) {
+                flFlashcard.setRotationY(0f);
+                tvWord.setRotationY(0f);
                 tvWord.setText(currentWord);
                 wordTranslation = translation.toLowerCase();
             }
